1412a82977edfbf1bf29bfa2690d9b366679b0b8
radamesvaz <rgvtovar@hotmail.com>
2025-02-03 12:58:15 -0600

    fixing sort merge

--- Algorithms/Sort/linked_list_merge_sort.js
+++ Algorithms/Sort/linked_list_merge_sort.js
@@ -54,50 +54,40 @@ const mergeLinkedList = (leftHalf, rightHalf) =>{
     // Obtain head nodes for left and right linked lists
     let leftHead = leftHalf.head
     let rightHead = rightHalf.head
-    console.log("leftHead")
-    console.log(leftHead)
-    console.log(rightHead)
     
     // Iterate over left & right until we reach the tail node of either
-    while(leftHead || rightHead){
-        console.log("current", current)
-        console.log("100")
+    while(leftHead !== null || rightHead !== null){
         // If the left head is null, add the nodes on the right to the current list
         if(leftHead === null){
-        console.log("200")
 
             current.nextNode = rightHead
             // since it will be a single linked list, only one value, by making the right head point to the next node
             // that is null, we will exit the loop
-            rightHead = rightHead.nextNode
+            break
         } else if(rightHead === null){
-            console.log("300")
 
             current.nextNode = leftHead
             // since it will be a single linked list, only one value, by making the left head point to the next node
             // that is null, we will exit the loop
-            leftHead = leftHead.nextNode
+            break
         }
-
-        // prepare the data for comparison
-        let leftData = leftHead.data
-        console.log(leftData)
-        let rightData = rightHead.data
 
         // Compare the values, add the node with the lesser value to the current.nextNode
         // and point to the next node of the comparison
-        if (leftData < rightData){
-            current.nextNode = leftHead
-            leftHead = leftHead.nextNode
+        if (leftHead.data < rightHead.data) {
+            current.nextNode = leftHead;
+            leftHead = leftHead.nextNode;
         } else {
-            current.nextNode = rightHead
-            rightHead = rightHead.nextNode
+            current.nextNode = rightHead;
+            rightHead = rightHead.nextNode;
         }
+
+        current = current.nextNode;
+
     }
 
     // Remove the fake head previously created
-    head = merged.head.next_node
-    merged.head = head
+    merged.head = merged.head.nextNode
 
     return merged
 
@@ -109,9 +99,7 @@ list.add(10)
 list.add(28)
 list.add(15)
 list.add(4)
-list.add(3)
-list.add(1)
-list.add(10)
+
 
 console.log(list.getList())
 const sortedList = mergeSortLinkedList(list)
